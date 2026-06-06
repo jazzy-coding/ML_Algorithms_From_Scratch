170c59bc69be0ffe7005d62a7b45d398d555a120
jazzy-coding <jasperwilson99@gmail.com>
2026-06-06 14:00:09 +0100

    add untested fit method to lin reg model

--- source/algorithms/linear_models/linear_regression.py
+++ source/algorithms/linear_models/linear_regression.py
@@ -1,29 +1,33 @@
 """Simple Linear Regression Model"""
 from typing import Callable, Optional
+import logging
+
 import numpy as np
+import numpy.typing as npt
+
 from source.loss.mean_squared_error import mean_squared_error
 
-class SimpleLinearRegression:
-    """Simple Linear Regression Model
+class LinearRegression:
+    """Linear Regression Model
 
     Model
     -----
-    y_hat    = (m * x) + c
+    y_hat    = (a * X) + b
 
     where
     -----
-    y_hat    = (1,): scalar prediction target variable (float)
-    m        = (1,): scalar gradient of the linear model (float)
-    x        = (1,): scalar predictor variable (float)
-    c        = (1,): scalar intercept of the linear model (float)
+    y_hat    = (n,): column vector of predicted targets (np.array)
+    a        = (m,): gradient vector of the coefficients in the linear model (np.array)
+    X        = (m, n): the feature matrix - m predictors to n data points (np.array)
+    b        = (1,): scalar intercept of the linear model (np.array)
 
-    1-dimensional linear regression is used when we have just 1 predictor
+    n-dimensional linear regression is used when we have n predictors
     in a regression problem. This method can only model linear relationships
     so it is best to do some exploratory data analysis first to make sure that
     the relationship between your predictor and your target is indeed approximately
-    linear. This also forms the base for n-dimensional linear regression.
+    linear. This also forms the base for polynomial regression, GLMs and so on.
 
-    Linear regression can also be thought of as the expected value of y given X - E(Y|X)
+    Linear regression can also be thought of as predicting the expected value of y given X - E(Y|X).
 
     Parameters:
     -----
@@ -36,37 +40,74 @@ class SimpleLinearRegression:
     epochs (int - defaults to 200): epochs represents the maximum number of gradient 
     descent iterations that will take place before the model training process exits.
 
+    random_state (int - defaults to 42): a random seed for experimental reproducibility
+
     Attributes:
     -----
     
-    coefficient_: np.array (1,) - representing 'm' in the above model formula.
+    coefficients_: np.array (m,) - representing 'a' in the above model formula.
 
-    intercept_: np.array (1,) - representing 'c' in the above model formula.
+    intercept_: np.array (1,) - representing 'b' in the above model formula.
 
     epochs_: int - the number of training epochs.
 
     learning_rate_: float - the learning rate of the gradient descent algorithm.
 
     loss_function_: Callable - the loss function used to evaluate the model.
+
+    random_state_: int - reproducibility seed
     """
 
     def __init__(
         self,
         learning_rate: Optional[float] = 0.01,
         epochs: int = 200,
-        loss: Callable = mean_squared_error
+        loss: Callable = mean_squared_error,
+        random_state: Optional[int] = 42
     ) -> None:
-        self.coefficient_ = None
+        self.coefficients_ = None
         self.intercept_ = None
         self.epochs_ = epochs
         self.learning_rate_ = learning_rate
         self.loss_ = loss
+        self.random_state_ = random_state
 
     def _net_input(self):
         pass
 
-    def fit(self):
-        pass
+    def fit(self, X: npt.NDArray, y: npt.NDArray) -> None:
+        """Fit a simple linear regression model with gradient descent.
+
+        NOTE: This only works with the mean squared error loss function at the moment.
+        Implementing derivatives soon!
+
+        Parameters:
+        -----
+        X (np.array): - (m, n) feature/predictor matrix of m features and n data points
+        y (np.array): - (n,) target value column vector of n data points
+
+        Returns:
+        -----
+        None
+        """
+        # initialise weights and biases randomly - drawing from a standard Gaussian distribution
+        self.coefficients_ = np.random.normal(loc=0.0, scale=1.0, size=X.shape[0])
+        self.intercept_ = np.array([0.])
+
+        for epoch in range(self.epochs_):
+            # 1. make predictions using current weights
+            preds = self.coefficients_ @ X + self.intercept_
+
+            # 2. compute the loss function with current weights and bias term
+            error = self.loss_(y=y, y_hat=preds)
+            
+            # 3. update the weights using gradient descent
+            self.coefficients_ = self.coefficients_ - (self.learning_rate_ * 2.0 * error)
+
+            # 4. log the epoch
+            logging.debug(f"Epoch: {epoch} - Loss: {error}")
+
+        logging.info("Model fitting complete.")
 
     def predict(self):
         pass
