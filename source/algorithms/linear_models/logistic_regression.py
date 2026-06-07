273a39838bba6e726877401957450d6b77c9cd40
jazzy-coding <jasperwilson99@gmail.com>
2026-06-07 20:34:38 +0100

    feat: implement constructor and methods for Logistic Regression class

--- source/algorithms/linear_models/logistic_regression.py
+++ source/algorithms/linear_models/logistic_regression.py
@@ -1,4 +1,10 @@
 """Logistic Regression Model"""
+from typing import Optional, Callable, List
+
+import numpy as np
+import numpy.typing as npt
+
+from source.loss.log_likelihood import log_likelihood
 
 class LogisticRegression:
     """Class that implements the Logistic Regression model.
@@ -58,4 +64,58 @@ class LogisticRegression:
 
     random_state
     """
+    def __init__(
+        self,
+        learning_rate: Optional[float] = 0.01,
+        epochs: Optional[int] = 200,
+        loss: Callable = log_likelihood,
+        random_state: Optional[int] = 42
+    ):
+        self.coefficients_ = np.array([])
+        self.intercept_ = np.array([])
+
+        self.learning_rate_ = learning_rate
+        self.epochs_ = epochs
+        self.loss_ = loss
+        self.random_state_ = random_state
+
+        self.losses_: List[float] = []
+
+
+    def net_input(self, X: npt.NDArray) -> npt.NDArray:
+        """Calculates the net input (W.T @ X + b) to the sigmoidal activation function.
+        
+        Parameters:
+        -----
+        X (np.array): - (m, n) a feature matrix of m features and n data points
+
+        Returns:
+        -----
+        np.array: - (n,) a column vector of net inputs from n data points
+        """
+        return self.coefficients_.dot(X.T) + self.intercept_
+
+    def fit(self, X: npt.NDArray, y: npt.NDArray) -> None:
+        """Fits the Logistic Regression model using standard gradient descent methods.
+        
+        Parameters:
+        -----
+        X (np.array): - (m, n) an input feature matrix of m features and n data points 
+        y (np.array): - (n,) a column vector of target values of length n data points
+
+        Returns:
+        -----
+        None
+        """
+        # reshape arrays for processing
+
+        # FOR EACH ITERATION:
+
+        # calculate net input
+        # calculate current loss and append
+        # update coefficients and intercept term
+        # repeat
+
+    def predict(self, X: npt.NDArray) -> npt.NDArray:
+        pass
     
